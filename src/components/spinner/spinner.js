704bc4004872926abceb514b9dcaddf51447fae3
Manola <blacklex2012@gmail.com>
2020-09-01 20:48:50 +0300

    first commit

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-pulse-r5d8b0npj9">
+            <div class="ldio-2xxy6mryi88">
+        <div></div><div></div><div></div>
+        </div></div>
+    )
+}
+
+export default Spinner;
